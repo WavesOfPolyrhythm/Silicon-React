77c8855b2d1d1f45990da41beb2f3c6fe08805c2
Tim <tim..newton.larsson@outlook.com>
2024-11-07 15:39:29 +0100

    added functionality for dark mode but I need to restructure my colors in root

--- src/components/DarkToggle.jsx
+++ src/components/DarkToggle.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+
+//I used Hans darkmode toggle segment combined with tips from Henrik Ljungberg to keep my css class .dark from my first project instead of adding data theme
 
 const DarkToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode)
+
+    if (newMode) {
+      document.body.classList.add('dark')
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark')
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme')
+
+    if(savedTheme === 'dark') {
+      document.body.classList.add('dark')
+      setIsDarkMode(true)
+    } else {
+      document.body.classList.remove('dark')
+      setIsDarkMode(false)
+    }
+  }, []);
+
+  
+
+
   return (
     <div id="darkmode-toggle" className="darkmode-toggle">
       <span className="label">Dark mode</span>
     <label htmlFor="darkmode-switch" className="toggle">
-      <input type="checkbox" id="darkmode-switch"/>
+      <input type="checkbox"
+       id="darkmode-switch"
+       checked={isDarkMode}
+       onChange={toggleDarkMode}/>
       <span className="slider"></span>
       </label>
   </div>
